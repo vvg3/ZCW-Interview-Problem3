7c777386f60220042182e49e2605677540de5f6d
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-04-15 13:10:45 -0400

    wu tang wutang

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,29 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+    public String wuTangWuTang(int limit) {
+
+        String wuTangAnswer = "";
+
+        for (int i = 1; i < limit+1; i++) {
+
+            if (i%3 == 0 && i%5 == 0) {
+                wuTangAnswer += "WuTang";
+            } else if (i%3 == 0) {
+                wuTangAnswer += "Wu";
+            } else if (i%5 == 0) {
+                wuTangAnswer += "Tang";
+            } else {
+                wuTangAnswer += i;
+            }
+            if (i < limit) {
+                wuTangAnswer += ", ";
+            }
+        }
+
+        return wuTangAnswer;
+    }
+
+
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,25 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Problem3Test {
+
+    private Problem3 tester;
+
+    @Before
+    public void setup() {
+        tester = new Problem3();
+    }
+
+    @Test
+    public void wuTangTest2() {
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang, " +
+                "16, 17, Wu, 19, Tang, Wu, 22, 23, Wu, Tang, 26, Wu, 28, 29, WuTang, 31, 32, " +
+                "Wu, 34, Tang, Wu, 37, 38, Wu, Tang, 41, Wu, 43, 44, WuTang, 46, 47, Wu, 49, Tang";
+        String actual = tester.wuTangWuTang(50);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
